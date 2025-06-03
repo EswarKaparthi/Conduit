d4a7a4429154b268becba20eb718c2ae15cb3eb0
Eswar Kaparthi <eswarkaparthi33@gmail.com>
2025-06-03 12:09:49 -0600

    Intaial commit

---
+++ cypress.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+
+  viewportHeight : 1080,
+  viewportWidth : 1920,
+  env : {
+    username: 'Cytest@mailinator.com',
+    password: 'Cytest',
+    apiUrl: 'https://conduit-api.bondaracademy.com/api',
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+
+    baseUrl: 'https://conduit.bondaracademy.com/',
+    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}',
+  },
+});
